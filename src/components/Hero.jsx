3465ace878fe65881af2f34cca4ece1aac844116
Atharva Dandgawhal <156278775+Atharva2116@users.noreply.github.com>
2024-11-14 20:39:41 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,8 +7,8 @@ function Hero() {
         <h1 className="text-5xl font-bold">Hi, I'm Atharva Dandgawhal</h1>
         <p className="text-xl mt-4">Bachelor of Engineering | Web Developer | Data Science Enthusiast</p>
         <a href="https://shorturl.at/Cjwg8" className="mt-6 inline-block bg-blue-500 px-6 py-3 rounded-full text-white hover:bg-blue-600">
-         <b> Get in Touch</b>  
-          <box-icon name='linkedin' type='logo' color='white'></box-icon>
+         <bold> Get in Touch</bold>  
+         
         </a>
       </div>
     </section>
