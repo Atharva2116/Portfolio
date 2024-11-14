a15e7ad5ed4ee5473a65e85f9737b924c2cec710
Atharva Dandgawhal <156278775+Atharva2116@users.noreply.github.com>
2024-11-14 23:48:14 +0530

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -43,11 +43,11 @@ function Projects() {
             </p>
             <div className="mt-6 grid grid-cols-2 gap-4">
              <img
-  src="./m1.jpeg"
-  alt="Moviez Project Screenshot 1"
-  className="w-full h-auto rounded-lg shadow-md cursor-pointer full-img"
-  onClick={() => handleImageClick('./m1.jpeg')}
-/>
+              src="/m1.jpeg"
+              alt="Moviez Project Screenshot 1"
+              className="w-full h-auto rounded-lg shadow-md cursor-pointer full-img"
+              onClick={() => handleImageClick('/m1.jpeg')}
+               />
 
               <img
                 src="../images/movie/m2.jpeg"
