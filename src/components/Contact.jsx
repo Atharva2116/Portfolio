b4f9a64178ce9225c1cf104ac626e67eeb650851
Atharva Dandgawhal <156278775+Atharva2116@users.noreply.github.com>
2024-11-16 12:15:29 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -18,7 +18,7 @@ function Contact() {
       const data = await response.json();
 
       if (data.success) {
-        setResult("Form Submitted Successfully");
+        setResult("Message Sent Successfully");
         e.target.reset(); // Reset the form fields after successful submission
       } else {
         console.log("Error", data);
@@ -26,7 +26,7 @@ function Contact() {
       }
     } catch (error) {
       console.log("Error", error);
-      setResult("There was an error submitting the form. Please try again.");
+      setResult("There was an error sending the message. Please try again.");
     }
   };
 
